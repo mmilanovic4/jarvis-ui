fbc67ca61172afa0b46a9fe114f9060b8c034b03
Miloš Milanović <mmilanovic016@gmail.com>
2026-05-11 20:39:23 +0200

    fix: revert

--- hooks/use-mobile.js
+++ hooks/use-mobile.js
@@ -3,23 +3,17 @@ import * as React from "react";
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(
-    () => window.innerWidth < MOBILE_BREAKPOINT,
-  );
+  const [isMobile, setIsMobile] = React.useState(undefined);
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
-
     mql.addEventListener("change", onChange);
-
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     return () => mql.removeEventListener("change", onChange);
   }, []);
-
-  console?.log({ isMobile: !!isMobile });
 
   return !!isMobile;
 }
